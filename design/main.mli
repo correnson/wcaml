872ec066efd22e5de0404842a91555f21beb09f4
Loïc Correnson <loic.correnson@cea.fr>
2013-04-18 13:30:40 +0200

    Main Design

--- design/main.mli
+++ design/main.mli
@@ -4,6 +4,9 @@ open Event
 (** {1 Main Entry Points} *)
 
 val init : appid:string -> unit (** To be called {i before} any call to the library. *)
-val run  : unit signal (** Runs the main event loop. *)
-val quit : unit signal (** Quits the main event loop. *)
+val main : unit -> unit (** Runs the main event loop. *)
+val quit : unit -> unit (** Quits the main event loop. *)
 
+val on_init : unit signal
+val on_main : unit signal
+val on_quit : unit signal
