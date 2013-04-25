2c887843be917a775709f808fc712654b0475143
Loïc Correnson <loic.correnson@cea.fr>
2013-04-25 14:36:57 +0200

    Add list and defaults to properties

--- design/property.ml
+++ design/property.ml
@@ -12,11 +12,33 @@ class bundle =
 object(self)
   val bundle : (int,Obj.t) Hashtbl.t = Hashtbl.create 31
   method id = Oo.id self
-  method get_prop : 'a. 'a key -> 'a 
-    = fun (k : 'a key) -> Obj.obj (Hashtbl.find bundle k)
-  method set_prop : 'a. 'a key -> 'a -> unit
-    = fun (k : 'a key) v -> Hashtbl.replace bundle k (Obj.repr v)
-  method remove_prop : 'a. 'a key -> unit
-    = fun (k : 'a key) -> Hashtbl.remove bundle k
+
+  method private access : 'a. 'a key -> 'a =
+    fun (k : 'a key) -> Obj.obj (Hashtbl.find bundle k)
+
+  method get_prop 
+    : 'a. (?default:'a -> ?exn:exn -> 'a key -> 'a)
+    = fun ?default ?(exn=Not_found) k ->
+      try self#access k
+      with Not_found ->
+	match default with
+	  | None -> raise exn
+	  | Some e -> e
+
+  method set_prop 
+    : 'a. 'a key -> 'a -> unit
+    = fun k v -> Hashtbl.replace bundle k (Obj.repr v)
+  method remove_prop 
+    : 'a. 'a key -> unit
+    = fun k -> Hashtbl.remove bundle k
+  method get_list 
+    : 'a. 'a list key -> 'a list
+    = fun k -> self#get_prop ~default:[] k
+  method append_list 
+    : 'a. 'a list key -> 'a -> unit
+    = fun k e -> self#set_prop k (self#get_list k @ [e])
+  method add_list 
+    : 'a. 'a list key -> 'a -> unit
+    = fun k e -> self#set_prop k (e :: self#get_list k)
 end
 
--- design/property.mli
+++ design/property.mli
@@ -7,7 +7,12 @@ val register : unit -> 'a key
 class bundle :
 object
   method id : int
-  method get_prop : 'a. 'a key -> 'a
+  method get_prop : 'a. 
+    ?default: 'a -> ?exn:exn -> 'a key -> 'a
+    (** Default exception if [Not_found]. *)
   method set_prop : 'a. 'a key -> 'a -> unit
+  method get_list : 'a. 'a list key -> 'a list (** Defaults to empty list *)
+  method add_list : 'a. 'a list key -> 'a -> unit
+  method append_list : 'a. 'a list key -> 'a -> unit
   method remove_prop : 'a. 'a key -> unit
 end
